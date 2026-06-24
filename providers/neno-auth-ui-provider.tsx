32f02f79f91aca3f2b7d782c158408725940e143
shubhamwankhede658 <shubhamwankhede658@gmail.com>
2026-06-24 18:45:12 +0530

    fix neon auth client url

--- providers/neno-auth-ui-provider.tsx
+++ providers/neno-auth-ui-provider.tsx
@@ -3,7 +3,7 @@
 import { createAuthClient } from "@neondatabase/auth";
 import { NeonAuthUIProvider } from "@neondatabase/auth/react";
 
-const authClient= createAuthClient();
+const authClient = createAuthClient(process.env.NEXT_PUBLIC_NEON_AUTH_BASE_URL!);
 
 export function NeonAuthProvider({children}:{children:React.ReactNode}){
     return(
