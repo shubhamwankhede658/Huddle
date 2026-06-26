6b246e766804c7a96fd8b6c54bb11a4db086e962
shubhamwankhede658 <shubhamwankhede658@gmail.com>
2026-06-26 20:52:37 +0530

    trying to fix some the cookie error

--- app/(core)/submit/page.tsx
+++ app/(core)/submit/page.tsx
@@ -3,7 +3,7 @@ import { getSessionUser } from "@/lib/auth";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 
-
+export const dynamic = "force-dynamic";
 export default async function SubmitPage(){
     const user = await getSessionUser();
     if(!user){
