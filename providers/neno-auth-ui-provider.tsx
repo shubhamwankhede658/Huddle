0e56b91d3fd8fce74b3eb7f488a4fa7f309b3521
shubhamwankhede658 <shubhamwankhede658@gmail.com>
2026-06-27 16:05:48 +0530

    updated neon auth ui provder tsx

--- providers/neno-auth-ui-provider.tsx
+++ providers/neno-auth-ui-provider.tsx
@@ -1,19 +1,26 @@
 "use client";
 
-import { createAuthClient } from "@neondatabase/auth";
+import { createAuthClient } from "@neondatabase/auth/next";  // ← change this import
 import { NeonAuthUIProvider } from "@neondatabase/auth/react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
-const authClient = createAuthClient(process.env.NEXT_PUBLIC_NEON_AUTH_BASE_URL!);
+const authClient = createAuthClient();  // ← no arguments
 
 export function NeonAuthProvider({children}:{children:React.ReactNode}){
+    const router = useRouter();
     return(
-        <NeonAuthUIProvider authClient={authClient} defaultTheme="dark" >
-          
+        <NeonAuthUIProvider 
+          authClient={authClient} 
+          defaultTheme="dark"
+          navigate={router.push}
+          replace={router.replace}
+          onSessionChange={() => router.refresh()}
+          redirectTo="/"
+          Link={Link}
+        >
             {children}
         </NeonAuthUIProvider>
     )
 }
-
-
-
 
